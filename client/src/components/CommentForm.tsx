3da091bdcc7aa28d70e662127a8421feb30a289d
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 22:40:42 +0200

    Refactored component names

---
+++ client/src/components/CommentForm.tsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Comments } from './Comments'
+
+type PetIdProps = {
+    petId: number;
+}
+
+type CommentDataProps = {
+    comment: string;
+}
+
+export const CommentForm = ({ petId }: PetIdProps) => {
+
+    const [comment, setComment] = useState('');
+    const [allComments, setAllComments] = useState([]);
+    const [commentCount, setCommentCount] = useState(0);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        postComment({comment});
+        setComment('');
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setComment(event.target.value);
+    }
+
+    const postComment = ({comment}: CommentDataProps) => {
+        axios.post('//localhost:8080/api/pets/' + petId + '/comments', {
+            message: comment
+        })
+        .then(response => {
+            console.log(response);
+            setCommentCount((commentCount: number) => commentCount + 1);
+        })
+        .catch((exception) => console.error(exception))
+    }
+
+    useEffect(() => {
+        axios.get('//localhost:8080/api/pets/' + petId + '/comments')
+        .then(response => {
+            setAllComments(response.data)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [commentCount, petId]);
+
+    return (
+        <>
+        <form className="" method="post" onSubmit={handleSubmit}>
+            <label className="form__label" htmlFor="newCommentForm">Leave a comment: </label>
+            <input type="text" className="form__input-name" value={comment} onChange={handleChange} placeholder="Enter Message" required />
+            <button type="submit" className="form__button-addDev">Submit</button>
+        </form>
+        <Comments allComments={allComments}/>
+        </>
+    )
+}
