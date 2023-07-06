386cca3a1b43c94d0dee482e772eb0b7ff1900db
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 20:32:18 +0200

    Created createComment()

--- src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetController.java
+++ src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetController.java
@@ -3,21 +3,22 @@ package se.pawsitive.vibes.pawsitivevibes.pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import se.pawsitive.vibes.pawsitivevibes.comment.Comment;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/pets")
 @CrossOrigin("*")
 public class PetController {
 
-    PetService service;
+    private final PetService service;
 
     @Autowired
     public PetController(PetService service) {
@@ -27,6 +28,16 @@ public class PetController {
     @GetMapping
     public List<Pet> getAllPets() {
         return service.getAllPets();
+    }
+
+    @GetMapping("/{petId}/comments")
+    public ResponseEntity<Object> getAllCommentsByPetId(@PathVariable Long petId) {
+        try {
+            List<Comment> comments = service.getAllCommentsByPetId(petId);
+            return ResponseEntity.ok(comments);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PostMapping
@@ -40,5 +51,12 @@ public class PetController {
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/{petId}/comments")
+    public ResponseEntity<Object> createComment(@PathVariable Long petId, @RequestBody String message) {
+        Comment comment = service.createComment(message, petId);
+        URI location = URI.create("/api/pets/{petId}/comments/" + comment.getId());
+        return ResponseEntity.created(location).body(comment);
     }
 }
