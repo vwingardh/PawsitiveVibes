f8b9c03af720065c567c9d4d1fa67d9f0b08c00a
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 09:26:29 +0200

    Updated Service with Comment repo

--- src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetService.java
+++ src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetService.java
@@ -3,6 +3,7 @@ package se.pawsitive.vibes.pawsitivevibes.pet;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.pawsitive.vibes.pawsitivevibes.comment.CommentRepository;
 
 import java.util.List;
 
@@ -11,11 +12,13 @@ import java.util.List;
 public class PetService {
 
     private final PetRepository petRepo;
+    private final CommentRepository commentRepo;
 
 
     @Autowired
-    public PetService(PetRepository petRepo) {
+    public PetService(PetRepository petRepo, CommentRepository commentRepo) {
         this.petRepo = petRepo;
+        this.commentRepo = commentRepo;
     }
 
     public List<Pet> getAllPets() {
