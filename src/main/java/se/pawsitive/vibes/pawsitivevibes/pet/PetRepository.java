fc9126840ee92f18c53c43639a55d57f3b5d0b54
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 20:32:40 +0200

    Created savePet()

--- src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetRepository.java
+++ src/main/java/se/pawsitive/vibes/pawsitivevibes/pet/PetRepository.java
@@ -2,24 +2,37 @@ package se.pawsitive.vibes.pawsitivevibes.pet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import se.pawsitive.vibes.pawsitivevibes.comment.JpaCommentRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Repository
 public class PetRepository {
 
-    private final JpaPetRepository repo;
+    private final JpaPetRepository petRepo;
+    private final JpaCommentRepository commentRepo;
 
     @Autowired
-    public PetRepository(JpaPetRepository repo) {
-        this.repo = repo;
+    public PetRepository(JpaPetRepository petRepo, JpaCommentRepository commentRepo) {
+        this.petRepo = petRepo;
+        this.commentRepo = commentRepo;
     }
 
     public List<Pet> getAllPets() {
-        return (List<Pet>) repo.findAll();
+        return (List<Pet>) petRepo.findAll();
+    }
+
+    public Pet getPetById(Long petId) throws NoSuchElementException {
+        Optional<Pet> retrievePet = petRepo.findById(petId);
+        if (retrievePet.isEmpty()) {
+            throw new NoSuchElementException("No pet exists with provided ID");
+        }
+        return retrievePet.get();
     }
 
     public Pet savePet(Pet newPet) {
-        return repo.save(newPet);
+        return petRepo.save(newPet);
     }
 }
