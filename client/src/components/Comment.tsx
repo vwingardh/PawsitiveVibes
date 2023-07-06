0a9618f9070c2508702f1ba6c57c454a6ca03e5a
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 17:25:58 +0200

    Created Comment component

---
+++ client/src/components/Comment.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Comment = () => {
+
+    // const getPetById = ({image, tag}: FormDataProps) => {
+        
+    //     axios.get('//localhost:8080/api/pets/' + id)
+    //     .then(response => {
+    //         console.log(response);
+    //     })
+    //     .catch((exception) => console.error(exception))
+    // }
+
+    return (
+    <div>DetailView</div>
+    )
+}
