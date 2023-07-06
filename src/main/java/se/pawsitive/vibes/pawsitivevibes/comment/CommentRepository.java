0b78e68e03fd9717b0bfcb1049b76cfa8d9d5117
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-06 20:31:39 +0200

    Created saveComment()

--- src/main/java/se/pawsitive/vibes/pawsitivevibes/comment/CommentRepository.java
+++ src/main/java/se/pawsitive/vibes/pawsitivevibes/comment/CommentRepository.java
@@ -2,6 +2,7 @@ package se.pawsitive.vibes.pawsitivevibes.comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import se.pawsitive.vibes.pawsitivevibes.pet.Pet;
 
 @Repository
 public class CommentRepository {
@@ -11,5 +12,9 @@ public class CommentRepository {
     @Autowired
     public CommentRepository(JpaCommentRepository repo) {
         this.repo = repo;
+    }
+
+    public Comment saveComment(Comment comment) {
+        return repo.save(comment);
     }
 }
