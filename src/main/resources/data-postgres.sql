3b7d9ab49076208f541a15cd24cc4871e960f57e
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-07 09:29:11 +0200

    Updated seeding database

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,3 +1,3 @@
-INSERT INTO pet (pet_img_path, pet_tag) VALUES ('', 'test') ON CONFLICT DO NOTHING;
-
-INSERT INTO comment (comment_message) VALUES ('test') ON CONFLICT DO NOTHING;
+INSERT INTO pet (pet_id, pet_img, pet_tag) VALUES (1, '/pet-uploads/catness.jpeg', 'cat') ON CONFLICT DO NOTHING;
+-- TRUNCATE TABLE pet CASCADE;
+INSERT INTO comment (comment_message, pet_id) VALUES ('Very cute', 1) ON CONFLICT DO NOTHING;
